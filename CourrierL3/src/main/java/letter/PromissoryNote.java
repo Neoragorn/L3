9cd05413bf502dcb10aaaddf82067a740751bbd3
neoragorn <casiersofian@gmail.com>
2015-11-19 22:48:49 +0100

    pl

---
+++ CourrierL3/src/main/java/letter/PromissoryNote.java
@@ -0,0 +1,28 @@
+package letter;
+
+
+import letter.NotUrgentLetter;
+
+/**
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+
+public class PromissoryNote extends NotUrgentLetter
+{
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 */
+	public PromissoryNote(){
+		super();
+	}
+
+	public void content()
+	{
+
+	}
+}
+
